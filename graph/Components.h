a0e5e0639d6ad059b6cbdc232fbcf2c4c2daf126
leaflc <zyx783353997@gmail.com>
2020-08-11 13:28:44 +0800

    增加图相关的算法

---
+++ graph/Components.h
@@ -0,0 +1,68 @@
+//
+// Created by leaflc on 2020/4/29.
+//
+
+#ifndef ALGORITHM_COMPONENTS_H
+#define ALGORITHM_COMPONENTS_H
+
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+template<typename Graph>
+class Components {
+
+private:
+    Graph &G;
+    bool *visited;
+    int ccount;
+    int *id;
+
+    void dfs(int v) {
+
+        visited[v] = true;
+        id[v] = ccount;
+        typename Graph::adjIterator adj(G, v);
+        for (int i = adj.begin(); !adj.end(); i = adj.next()) {
+            if (!visited[i])
+                dfs(i);
+        }
+    }
+
+public:
+    Components(Graph &graph) : G(graph) {
+
+        visited = new bool[G.V()];
+        id = new int[G.V];
+        ccount = 0;
+        for (int i = 0; i < G.V(); ++i) {
+            visited[i] = false;
+            id[i] = -1;
+        }
+
+        for (int i = 0; i < G.V(); ++i) {
+            if (!visited[i]) {
+                dfs(i);
+                ccount++;
+            }
+        }
+    }
+
+    ~Components() {
+        delete[]  visited;
+        delete[] id;
+    }
+
+    int count() {
+        return ccount;
+    }
+
+    bool isConnected(int v, int w) {
+        assert(v >= 0 && v < G.V);
+        assert(w >= 0 && v < G.V);
+        return id[v] == id[w];
+    }
+};
+
+#endif //ALGORITHM_COMPONENTS_H
--- main.cpp
+++ main.cpp
@@ -1,10 +1,57 @@
 #include <iostream>
 #include "union_find/UnionFindTestHelper.h"
+#include "graph/SpaseGraph.h"
+#include "graph/DenseGraph.h"
+#include "graph/ReadGraph.h"
+#include "graph/Components.h"
 
 using namespace std;
 
-int main() {
 
+void graphTest() {
+    int N = 20;
+    int M = 100;
+
+    srand(time(NULL));
+
+    // Sparse Graph
+    SparseGraph g1(N, false);
+    for (int i = 0; i < M; ++i) {
+        int a = rand() % N;
+        int b = rand() % N;
+        g1.addEdge(a, b);
+    }
+
+    for (int v = 0; v < N; ++v) {
+        cout << v << " : ";
+        SparseGraph::adjIterator adj(g1, v);
+        for (int w = adj.begin(); !adj.end(); w = adj.next()) {
+            cout << w << " ";
+        }
+        cout << endl;
+    }
+
+    // Dense Graph
+    DenseGraph g2(N, false);
+    for (int i = 0; i < M; ++i) {
+        int a = rand() % N;
+        int b = rand() % N;
+        g2.addEdge(a, b);
+    }
+
+    for (int v = 0; v < N; ++v) {
+        cout << v << " : ";
+        DenseGraph::adjIterator adj(g2, v);
+        for (int w = adj.begin(); !adj.end(); w = adj.next()) {
+            cout << w << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+
+void unionFindTest() {
     int n = 1000000;
 
     UnionFindTestHelper::testUF1(n);
@@ -12,5 +59,15 @@ int main() {
     UnionFindTestHelper::testUF3(n);
     UnionFindTestHelper::testUF4(n);
     UnionFindTestHelper::testUF5(n);
+}
+
+int main() {
+
+    // TestG1.txt - g1 and g2
+    string filename1 = "testG1.txt";
+    SparseGraph g1 = SparseGraph(13, false);
+    ReadGraph<SparseGraph> readGraph1(g1, filename1);
+    Components<SparseGraph> component1(g1);
+    cout<<"TestG1.txt, Using Sparse Graph, Components Count: "<<component1.count()<<endl;
     return 0;
 }
